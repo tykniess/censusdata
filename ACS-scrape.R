67cad1c1461efb53d5d65764ef12e2180498d9aa
Tyler Kniess <tyler.kniess@TylerKniess-1850-13MBPa.Prolific>
2024-01-26 11:31:51 -0500

    Update initial data.frame to make it cleaner

--- ACS-scrape.R
+++ ACS-scrape.R
@@ -19,20 +19,9 @@ library(curl)
 #Must enter individual key
 my_key=""
 
-variable_labels = t(data.frame(
-  c("Variable_Code","Variable_Name"),
-  c("NAME","Place_Name"),
-  c("B01001_001E","Estimate!!Total:"),
-  c("B01001_011E","Estimate!!Total:!!Male:!!25 to 29 years"),
-  c("B01001_012E","Estimate!!Total:!!Male:!!30 to 34 years"),
-  c("B01001_013E","Estimate!!Total:!!Male:!!35 to 39 years"),
-  c("B01001_014E","Estimate!!Total:!!Male:!!40 to 44 years"),
-  c("B01001_035E","Estimate!!Total:!!Female:!!25 to 29 years"),
-  c("B01001_036E","Estimate!!Total:!!Female:!!30 to 34 years"),
-  c("B01001_037E","Estimate!!Total:!!Female:!!35 to 39 years"),
-  c("B01001_038E","Estimate!!Total:!!Female:!!40 to 44 years"), row.names = TRUE
-)
-)
+variable_labels <- data.frame (var_id=c("NAME","B01001_001E","B01001_011E","B01001_012E","B01001_013E","B01001_014E","B01001_035E","B01001_036E","B01001_037E","B01001_038E"), 
+                                      var_name=c("Place_name","Estimate!!Total:","Estimate!!Total:!!Male:!!25 to 29 years","Estimate!!Total:!!Male:!!30 to 34 years","Estimate!!Total:!!Male:!!35 to 39 years","Estimate!!Total:!!Male:!!40 to 44 years","Estimate!!Total:!!Female:!!25 to 29 years","Estimate!!Total:!!Female:!!30 to 34 years","Estimate!!Total:!!Female:!!35 to 39 years","Estimate!!Total:!!Female:!!40 to 44 years"))
+
 
 #Build the url for scraping.
 prefix <- "https://api.census.gov/data/2022/acs/acs5?get="
